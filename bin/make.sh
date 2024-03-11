d037ad28ab9a3d8768eda6df268701db30cfa53b
Roming22 <rarnaud@redhat.com>
2024-03-11 17:11:16 -0400

    Fix DH TLS reconfiguration bug (#40)
    Once TLS had been configured once, the process was failing because
    it did not account for the existing configuration.
    Since 'oc patch' does not seem great to handle this scenario, 'yq'
    is used to set/update the values.

--- bin/make.sh
+++ bin/make.sh
@@ -171,7 +171,7 @@ release() {
   git commit -m "Release $version
   
   Changes from $previous_version:
-  $(
+$(
     git -C "$HELM_CHART" log \
       --reverse --format="- %s" "$previous_version^..$version" \
       -- Chart.yaml values.yaml templates |
